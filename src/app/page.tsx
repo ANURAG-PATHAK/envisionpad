0681a62e79047cbdcd738ba5e59d0bbebe531c5e
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-08 21:36:47 +0530

    feat: Add Header and Hero components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
-
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 export default function Home() {
   return (
-    <div className="text-6xl">
-      <Button>Button</Button>
-    </div>
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 }
