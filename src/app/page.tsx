519614ef0ee3ccc69d30fe156a642d129b721953
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-08 21:16:12 +0530

    chore: Update package.json and package-lock.json with @radix-ui/react-slot v1.1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="text-6xl">visionPad</div>
+    <div className="text-6xl">
+      <Button>Button</Button>
+    </div>
   );
 }
