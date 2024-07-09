b4f290633bbc5c3ba121ba5aec563c5db02b0e8c
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-09 05:44:43 +0530

    chore: Update package.json with @radix-ui/react-navigation-menu v1.2.0 dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,44 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+import Image from "next/image"
+import { Button } from "./ui/button"
+import Link from "next/link"
+
 export default function Header() {
-    return (
-        <div>Header</div>
-    )
+  return (
+    <nav className="flex justify-between items-center">
+      <div className="flex items-center">
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <div>Envision Pad</div>
+      </div>
+      <div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href={"/"} className={navigationMenuTriggerStyle()}>pricing</Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div>
+        <Button>Login</Button>
+        <Button variant={"secondary"}>Register</Button>
+      </div>
+    </nav>
+  )
 }
