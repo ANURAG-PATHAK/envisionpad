033c714d3f648abd7204b75ef691504bd9afa70e
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-09 20:04:00 +0530

    chore: Update package.json and package-lock.json with react-icons v5.2.1 dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,71 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+"use client";
 import Image from "next/image"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { MdMenu, MdClose } from "react-icons/md";
+import { useState } from "react";
+import clsx from "clsx";
 
 export default function Header() {
   return (
-    <nav className="flex justify-between items-center">
-      <div className="flex items-center">
-        <Image src="/logo.png" alt="Logo" width={40} height={40} />
-        <div>Envision Pad</div>
-      </div>
+    <>
+      <DesktopNav className="hidden md:flex" />
+      <MobileNav className="flex md:hidden" />
+    </>
+  )
+}
+
+export function DesktopNav({ className }: { className: string }) {
+  return (
+    <div className={clsx(className, "container flex justify-between items-center mx-6 my-2")}>
+      <Link href="/" className="flex items-center">
+        <Image src="/logo.png" alt="Logo" width={50} height={50} />
+        <div className="mx-2 text-lg">Envision Pad</div>
+      </Link>
+      <nav className="flex justify-between my-4">
+        <Link className="mx-2" href="/">Home</Link>
+        <Link className="mx-2" href="/about">About</Link>
+        <Link className="mx-2" href="/contact">Contact</Link>
+      </nav>
       <div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href={"/"} className={navigationMenuTriggerStyle()}>pricing</Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <Button className="mx-2">Login</Button>
+        <Button className="mx-2" variant={"secondary"}>Register</Button>
       </div>
-      <div>
-        <Button>Login</Button>
-        <Button variant={"secondary"}>Register</Button>
+    </div>
+  )
+}
+
+export function MobileNav({ className }: { className: string }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className={clsx(className, "container flex flex-col my-4")}>
+      <div className={isOpen ? "hidden" : "flex justify-between md:hidden"}>
+        <Link href="/" className="flex items-center">
+          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <div className="mx-2">Envision Pad</div>
+        </Link>
+        <div className="">
+          <Button variant={"ghost"} onClick={() => { setIsOpen(true) }} >
+            <MdMenu className="text-2xl" />
+          </Button>
+        </div>
+      </div >
+      <div className={isOpen ? "flex flex-col md:hidden" : "hidden"}>
+        <div className="self-end">
+          <Button variant={"ghost"} onClick={() => { setIsOpen(false) }}>
+            <MdClose className="text-2xl" />
+          </Button>
+        </div>
+        <nav className="flex flex-col items-center my-4">
+          <Link className="my-2" href="/">Home</Link>
+          <Link className="my-2" href="/about">About</Link>
+          <Link className="my-2" href="/contact">Contact</Link>
+        </nav>
+        <div className="flex flex-col my-4">
+          <Button className="my-2">Login</Button>
+          <Button className="my-2" variant={"secondary"}>Register</Button>
+        </div>
       </div>
-    </nav>
+    </div >
   )
 }
