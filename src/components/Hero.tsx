06a20e2de96ff60f05f8b1089de512df43d2f3c8
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-13 06:23:24 +0530

    updated hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,12 +5,12 @@ export default function Hero() {
     <div className="container mx-2 my-4 mt-44">
       <div className="flex flex-col space-y-8 md:space-x-12">
         <div className="flex flex-col items-center justify-center">
-          <div className="text-4xl md:text-6xl text-[#3B81F6]">Collaborate & Create</div>
-          <div className="text-4xl md:text-6xl">for brainstorming sessions</div>
+          <div className="text-4xl md:text-6xl text-[#3B81F6] text-center">Collaborate & Create</div>
+          <div className="text-4xl md:text-6xl text-center">for brainstorming sessions</div>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <div className="text-xl md:text-3xl">All-in-one markdown editor, collaborative canvas, </div>
-          <div className="text-xl md:text-3xl">and diagram-as-code builder</div>
+          <div className="text-xl md:text-3xl text-center">All-in-one markdown editor, collaborative canvas, </div>
+          <div className="text-xl md:text-3xl text-center">and diagram-as-code builder</div>
         </div>
         <div className="self-center">
           <Button>Get Started <MdArrowForward className="mx-1 text-lg" /></Button>
